d2005ae970508ad775d9a7819279e2468eeb7429
rufaid-safcodes <rufaid@safcodes.com>
2025-08-13 16:52:09 +0530

    category form feature image add webp format and png format less than 1 mb

--- src/views/app-views/dashboards/categories/CategoryForm/GeneralField.js
+++ src/views/app-views/dashboards/categories/CategoryForm/GeneralField.js
@@ -57,15 +57,17 @@ const iconUploadProps = {
 };
 
 const beforeUpload = (file) => {
-  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-  if (!isJpgOrPng) {
-    message.error("You can only upload JPG/PNG file!");
+  const isPngOrWebp = file.type === "image/png" || file.type === "image/webp";
+  if (!isPngOrWebp) {
+    message.error("You can only upload PNG or WebP files!");
+    return false;
   }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error("Image must smaller than 2MB!");
+  const isLt1M = file.size / 1024 / 1024 < 1;
+  if (!isLt1M) {
+    message.error("Image must be smaller than 1MB!");
+    return false;
   }
-  return isJpgOrPng && isLt2M;
+  return true;
 };
 
 const beforeIconUpload = (file) => {
